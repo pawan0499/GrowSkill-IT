eba35ea5aba16454b3aaed56bda5a0895140a83c
unknown <pavanmali4299@gmail.comgit config --global user.email pavanmali4299@gmail.com>
2025-12-23 22:38:37 +0530

    initial

---
+++ Assignment1.java
@@ -0,0 +1,23 @@
+package JavaAssignment;
+
+public class Assignment1 {
+
+	public static void main(String[] args) 
+	
+	{
+//		1st Assignment
+		
+	   int a = 22;
+	   System.out.println(a);
+	   
+	   float height = 170.5f;
+	   System.out.println(height);
+	    
+	   long num = 9876543210L;
+	   System.out.println(num);
+	   
+	   boolean Student = true;
+	   System.out.println(Student);
+	}
+
+}
---
+++ Assignment3.java
@@ -0,0 +1,23 @@
+package JavaAssignment;
+
+public class Assignment3 {
+
+	public static void main(String[] args) 
+	{
+	 
+		// 3rd Assignment
+		
+		int subject1 = 75;
+		int subject2 = 68;
+		int subject3 = 82;
+		boolean pass = true;
+		  
+		
+		System.out.println("Subject 1 marks: " + subject1);
+		System.out.println("Subject 2 marks: " + subject2);
+		System.out.println("Subject 3 marks: " + subject3);
+		System.out.println("Student pass:- " + pass);
+
+	}
+
+}
