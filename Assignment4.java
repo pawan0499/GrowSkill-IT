c9bac5993d7cc510d3679849ded4eb9856513569
unknown <pavanmali4299@gmail.comgit config --global user.email pavanmali4299@gmail.com>
2025-12-26 12:40:33 +0530

    Assignment

---
+++ Assignment4.java
@@ -0,0 +1,19 @@
+package JavaAssignment;
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 25;
+		int num2 = 30;
+		
+		if(num1 < num2) {
+			System.out.println(num1 + " number is smallest");
+		}
+		else 
+		{
+			System.out.println(num2 + " number is smallest");	
+	    }
+	}
+
+}
---
+++ Assignment5.java
@@ -0,0 +1,20 @@
+package JavaAssignment;
+
+public class Assignment5 {
+	
+	public static void main(String[] args) {
+		int num1 = 55;
+//		int num2 = 45;
+//		int num3 = 35;
+		
+		if(num1)
+		{
+			System.out.println();	
+		}
+		else 
+		{
+			System.out.println();
+		}
+	}
+
+}
